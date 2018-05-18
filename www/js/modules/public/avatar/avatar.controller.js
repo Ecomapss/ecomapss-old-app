09bf2ded8d1fd50f96a12188e3e8f658d2e2eb9f
Matheus Soares <mathe_oare@hotmail.com>
2018-05-18 13:10:42 -0300

    avatar view almost done

--- www/js/modules/public/avatar/avatar.controller.js
+++ www/js/modules/public/avatar/avatar.controller.js
@@ -9,8 +9,19 @@
   function AvatarCtrl(I18nService){
     var vm = this;
     var thisModule = 'avatar';
+    var avatars = {ext: '.png', max: 6};
+    var prefix = '';
     vm.thisLocation = I18nService.getLang();
     vm.user = {};
+    vm.avatars = [];
+
+    for (var i = 1; i <= avatars.max; i++) {
+      if (i>10) prefix = '0';
+      else prefix = '00';
+      vm.avatars.push({
+        file: prefix + i + avatars.ext, id: prefix+i, marked: false
+      });
+    }
 
     init();
 
@@ -19,6 +30,13 @@
       I18nService.geti18n(vm.thisLocation).then(function (response) {
         vm.translate = response.data[thisModule];
       });
+    }
+
+    vm.choose = function(id) {
+      vm.avatars.map(function (avatar) {
+        if (id == avatar.id) avatar.marked = !avatar.marked;
+        else avatar.marked = false;
+      })
     }
   }
 }());
