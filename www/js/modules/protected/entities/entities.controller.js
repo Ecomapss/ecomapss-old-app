28025f8443a22e2d51e02762069cbc1c09cff6d2
Matheus Soares <mathe_oare@hotmail.com>
2018-05-20 17:34:01 -0300

    added more entity

--- www/js/modules/protected/entities/entities.controller.js
+++ www/js/modules/protected/entities/entities.controller.js
@@ -2,21 +2,23 @@
     'use strict';
 
     angular
-        .module('ecomapss.protected')
-        .controller('EntitiesCtrl', EntitiesCtrl)
+    .module('ecomapss.protected')
+    .controller('EntitiesCtrl', EntitiesCtrl)
 
     /** @ngInject */
-    function EntitiesCtrl($ionicSideMenuDelegate,$ionicSlideBoxDelegate){
+    function EntitiesCtrl($ionicSideMenuDelegate,$ionicSlideBoxDelegate, I18nService){
         var vm = this;
+        var thisModule = 'entities';
+        vm.thisLocation = I18nService.getLang();
         
         init();
 
         function init(){
-        }
-
-
-
-
-    }
+          vm.langOptions = I18nService.getLangOptions(vm.thisLocation);
+          I18nService.geti18n(vm.thisLocation).then(function (response) {
+            vm.translate = response.data[thisModule];
+        });
+      }
+  }
 
 }());
