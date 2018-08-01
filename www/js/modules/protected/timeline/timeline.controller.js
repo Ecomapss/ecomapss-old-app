fe1041240e27f071fdc49a558baded9af2e96ba5
Matheus Soares <mathe_oare@hotmail.com>
2018-08-01 12:56:12 -0300

    refresh timeline added

--- www/js/modules/protected/timeline/timeline.controller.js
+++ www/js/modules/protected/timeline/timeline.controller.js
@@ -15,6 +15,18 @@
         vm.itemsIsFetched = false
         vm.thisLocation = I18nService.getLang();
 
+        vm.doRefresh = function () {
+            histories = TimelineService.getHistories();
+            if (histories === ecConstants.ERRORS.HISTORIES_NOT_FOUND) {
+
+            } else {
+                histories = JSON.parse(histories);
+                vm.fetchedHistories = angular.copy(histories);
+            }
+            $timeout(function () {
+                $scope.$broadcast("scroll.refreshComplete");
+            }, 1000);
+        }
 
         init();
 
@@ -22,22 +34,14 @@
             vm.itemsIsFetched = true;
         }, 3500)
 
+
         function init() {
             vm.langOptions = I18nService.getLangOptions(vm.thisLocation);
             I18nService.geti18n(vm.thisLocation).then(function (response) {
                 vm.translate = response.data[thisModule];
             });
 
-            histories = TimelineService.getHistories();
-
-            if (histories === ecConstants.ERRORS.HISTORIES_NOT_FOUND) {
-
-            } else {
-                histories = JSON.parse(histories);
-                vm.fetchedHistories = angular.copy(histories);
-
-                console.log(vm.fetchedHistories);
-            }
+            vm.doRefresh();
         }
 
     }
