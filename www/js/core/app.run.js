28a632fd66c9731f79e1cc3f11e2d250c3f32931
Matheus Soares <mathe_oare@hotmail.com>
2018-08-15 21:13:22 -0300

    atualizações no mapa

--- www/js/core/app.run.js
+++ www/js/core/app.run.js
@@ -2,8 +2,8 @@
   'use strict';
 
   angular
-    .module('ecomapss')
-    .run(ecomapssRun)
+  .module('ecomapss')
+  .run(ecomapssRun)
 
   /** @ngInject */
   function ecomapssRun($ionicPlatform, UserService, $state, I18nService, $timeout, routerHelper) {
@@ -46,82 +46,7 @@
           StatusBar.styleLightContent();
         }
         ionic.Platform.fullScreen(true, true);
-      }
-
-      if (window.BackgroundGeolocation) {
-        var bgGeo = window.BackgroundGeolocation;
-
-        //This callback will be executed every time a geolocation is recorded in the background.
-        var callbackFn = function (location) {
-          var coords = location.coords;
-          var lat = coords.latitude;
-          var lng = coords.longitude;
-          console.log('- Location: ', JSON.stringify(location));
-        };
-
-        // This callback will be executed if a location-error occurs.  Eg: this will be called if user disables location-services.
-        var failureFn = function (errorCode) {
-          console.warn('- BackgroundGeoLocation error: ', errorCode);
-        }
-
-        // Listen to location events & errors.
-        bgGeo.on('location', callbackFn, failureFn);
-        // Fired whenever state changes from moving->stationary or vice-versa.
-        bgGeo.on('motionchange', function (isMoving) {
-          console.log('- onMotionChange: ', isMoving);
-        });
-        // Fired whenever a geofence transition occurs.
-        bgGeo.on('geofence', function (geofence) {
-          console.log('- onGeofence: ', geofence.identifier, geofence.location);
-        });
-        // Fired whenever an HTTP response is received from your server.
-        bgGeo.on('http', function (response) {
-          console.log('http success: ', response.responseText);
-        }, function (response) {
-          console.log('http failure: ', response.status);
-        });
-
-        // BackgroundGeoLocation is highly configurable.
-        bgGeo.ready({
-          // Geolocation config
-          desiredAccuracy: 0,
-          distanceFilter: 10,
-          stationaryRadius: 25,
-          // Activity Recognition config
-          activityRecognitionInterval: 10000,
-          stopTimeout: 5,
-          // Application config
-          debug: true,  // <-- Debug sounds & notifications.
-          stopOnTerminate: false,
-          startOnBoot: true,
-          // HTTP / SQLite config
-          url: "http://your.server.com/locations",
-          method: "POST",
-          autoSync: true,
-          maxDaysToPersist: 3,
-          headers: {  // <-- Optional HTTP headers
-            "X-FOO": "bar"
-          },
-          params: {   // <-- Optional HTTP params
-            "auth_token": "maybe_your_server_authenticates_via_token_YES?"
-          }
-        }, function (state) {
-          // This callback is executed when the plugin is ready to use.
-          console.log("BackgroundGeolocation ready: ", state);
-          if (!state.enabled) {
-            bgGeo.start();
-          }
-        });
-
-        // The plugin is typically toggled with some button on your UI.
-        function onToggleEnabled(value) {
-          if (value) {
-            bgGeo.start();
-          } else {
-            bgGeo.stop();
-          }
-        }
-      }
+      }  
     });
   }
 }());
--- www/js/directives/map/directive.js
+++ www/js/directives/map/directive.js
@@ -6,28 +6,43 @@
         .directive('ecMap', mapDirective);
 
     /* @ngInject */
-    function mapDirective($cordovaGeolocation, $qs, $rootScope, $ionicPlatform, $timeout) {
+    function mapDirective($cordovaGeolocation, $q, $rootScope, $ionicPlatform, $timeout) {
         var directive = {
+            scope: {
+                onCreate: "&"
+            },
             link: link,
-            templateUrl: 'js/directives/map/template.html',
+            // templateUrl: 'js/directives/map/template.html',
             restrict: 'EA'
         };
 
         var watchProps = {
-            timeout: 6000,
+            timeout: 10000,
             enableHighAccuracy: true
         }
 
         return directive;
 
         function link(scope, element, attrs) {
+
             $ionicPlatform.ready(function () {
-                $cordovaGeolocation.watchPosition(watchProps).then(null, function (err) {
-                    alert(err.toString());
-                }, function (position) {
+                $cordovaGeolocation.getCurrentPosition(watchProps).then(function (res) {
+                    var mapOptions = {
+                        center: [res.coords.latitude, res.coords.longitude],
+                        zoom: 15,
+                        zoomControl: true,
+                        maxZoom: 18,
+                        minZoom: 9
+                    };
                     $timeout(function () {
-                        scope.posis = position.coords.latitude + " <> " + position.coords.longitude
+                        var map = L.map(element[0], mapOptions);
+                        L.tileLayer("tiles/{z}/{x}/{y}.png", {
+                            attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                        }).addTo(map);
+                        scope.onCreate({ map: map, L: L });
                     }, 0)
+                }, function(err) {
+                    alert("Error ao recuperar localização, por favor verifique o seu gps está ativado ou se o ecomapss tem as permissões adequadas.")
                 })
             })
         }
